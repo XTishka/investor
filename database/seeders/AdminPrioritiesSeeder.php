99b641488bcd19a01e5b44e2dc9817273c7cd657
Takhir Berdyiev <takhir.berdyiev@gmail.com>
2022-06-03 12:43:59 +0300

    Add seeder to priorities
    - add Priority model
    - add dependencies to User (rounds, priorities)
    - add priority seeds to default users

---
+++ database/seeders/AdminPrioritiesSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Priority;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminPrioritiesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Priority::create([
+            'user_id' => 1,
+            'round_id' => 1,
+            'priority' => 1,
+            'available_weeks' => 10
+        ]);
+
+        Priority::create([
+            'user_id' => 2,
+            'round_id' => 1,
+            'priority' => 2,
+            'available_weeks' => 5
+        ]);
+
+        Priority::create([
+            'user_id' => 3,
+            'round_id' => 1,
+            'priority' => 3,
+            'available_weeks' => 3
+        ]);
+    }
+}

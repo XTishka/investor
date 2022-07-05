84889a25db103e54321da3d1aeed4ce948992a8c
Takhir Berdyiev <takhir.berdyiev@gmail.com>
2022-07-05 17:44:15 +0300

    Add mail translations for new users

--- lang/dk/mail_new_user.php
+++ lang/dk/mail_new_user.php
@@ -1,11 +1,12 @@
 <?php
 return [
-    'greeting' => 'Hej',
-    'message_body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias aperiam aliquam laboriosam facere dolore doloremque molestias perferendis? Repudiandae, facilis. Nobis eum quos tenetur aliquam. Quis quod illo saepe expedita pariatur.',
+    'greeting' => 'Kære aktionær,',
+    'message_body' => 'Her er dine loginoplysninger til vores prioriteringssystem, hvor du kan indtaste op til 20 ønsker i den kommende prioriteringsrunde.
+    Du har modtaget oplysninger omkring boligerne i en særskilt mail, og kan desuden se vores boliger på hjemmesiden.',
     'login_name' => 'Login-navn',
     'password' => 'Adgangskode',
     'login_button' => 'Log på',
-    'reset_button_description' => 'Eller du kan konfigurere din adgangskode her:',
+    'reset_button_description' => 'Koden kan nemt skiftes ved at trykke på "nulstille kodeord" knappen.',
     'reset_button' => 'Nulstille kodeord',
-    'thanks' => 'Tak',
+    'thanks' => 'Vi ser frem til at modtage dine ønsker.',
 ];
--- lang/en/mail_new_user.php
+++ lang/en/mail_new_user.php
@@ -1,11 +1,13 @@
 <?php
 return [
-    'greeting' => 'Hello',
-    'message_body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias aperiam aliquam laboriosam facere dolore doloremque molestias perferendis? Repudiandae, facilis. Nobis eum quos tenetur aliquam. Quis quod illo saepe expedita pariatur.',
+    'greeting' => 'Dear stockholder, ',
+    'message_body' => 'Here is your login information for our prioritization system, where you can enter up to 20 wishes in the upcoming prioritization round.<br>
+    You have received information about the homes in a separate email, and can also see our homes on the website.
+    You must use the email and password below when logging in.',
     'login_name' => 'Login name',
     'password' => 'Password',
     'login_button' => 'Login',
-    'reset_button_description' => 'Or you can setup your password here:',
+    'reset_button_description' => 'The code can be easily changed by pressing the "reset password" button.',
     'reset_button' => 'Reset password',
-    'thanks' => 'Thanks',
+    'thanks' => 'We look forward to receiving your wishes.',
 ];

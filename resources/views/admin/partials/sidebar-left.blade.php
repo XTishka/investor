4415db923a2fe05fd648a3a8fcae8b01d0b82705
Takhir Berdyiev <takhir.berdyiev@gmail.com>
2022-07-14 09:26:08 +0300

    JETSTREAM REBORN :: clean directory

--- resources/views/admin/partials/sidebar-left.blade.php
+++
@@ -1,8 +0,0 @@
-<aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-    <p class="p-3">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto expedita minus nam, nostrum quidem
-        tempore. Cupiditate eius labore, laborum magnam maiores nesciunt nostrum obcaecati, porro possimus rem similique
-        temporibus.
-    </p>
-</aside>

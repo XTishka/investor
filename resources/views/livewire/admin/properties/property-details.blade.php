e003d5af2b12c0f2b72d775de342c5b6672e03e9
Takhir Berdyiev <takhir.berdyiev@gmail.com>
2022-12-04 18:03:35 +0200

    Fix table display if round is not selected

--- resources/views/livewire/admin/properties/property-details.blade.php
+++ resources/views/livewire/admin/properties/property-details.blade.php
@@ -19,86 +19,96 @@
     <x-slot name="data">
 
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <table class="w-full">
-                <thead>
-                    <tr class="font-bold text-xs uppercase text-black">
-                        <th scope="col" class="px-6 py-3 bg-gray-50 text-left tracking-wider">
-                            #
-                        </th>
+            @if ($roundWeeks)
+                <table class="w-full">
+                    <thead>
+                        <tr class="font-bold text-xs uppercase text-black">
+                            <th scope="col" class="px-6 py-3 bg-gray-50 text-left tracking-wider">
+                                #
+                            </th>
 
-                        <th scope="col" class="px-6 py-3 bg-gray-50 tracking-wider text-center">
-                            {{ __('Week') }}
-                        </th>
+                            <th scope="col" class="px-6 py-3 bg-gray-50 tracking-wider text-center">
+                                {{ __('Week') }}
+                            </th>
 
 
-                        <th scope="col" class="px-6 py-3 bg-gray-50 text-left tracking-wider"></th>
+                            <th scope="col" class="px-6 py-3 bg-gray-50 text-left tracking-wider"></th>
 
-                        <th scope="col" class="px-6 py-3 bg-gray-50 text-right tracking-wider">
-                            {{ __('Status') }}
-                        </th>
-                    </tr>
-                </thead>
-
-                <tbody class="bg-white divide-y divide-gray-200">
-                    @foreach ($roundWeeks as $week)
-                        <tr>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                {{ $loop->iteration }}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 font-bold text-center">
-                                # {{ $week['week_start']['number'] }}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-xs text-gray-600 ml-4 text-center">
-                                {{ $week['week_start']['human_date'] }} - {{ $week['week_end']['human_date'] }}
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 text-right flex justify-end">
-                                @if ($week['week_start']['status'] === true and $week['week_end']['status'] === false)
-                                    <x-admin.elements.badge-disabled>
-                                        {{ __('passed') }}
-                                    </x-admin.elements.badge-disabled>
-                                @elseif ($week['week_start']['status'] === true and $week['week_end']['status'] === true)
-                                    <x-admin.elements.badge-success>
-                                        {{ __('current') }}
-                                    </x-admin.elements.badge-success>
-                                @elseif ($week['week_start']['status'] === false and $week['week_end']['status'] === true)
-                                    <x-admin.elements.badge-primary>
-                                        {{ __('waiting') }}
-                                    </x-admin.elements.badge-primary>
-                                @else
-                                    <x-admin.elements.badge-error>
-                                        {{ __('error') }}
-                                    </x-admin.elements.badge-error>
-                                @endif
-
-                                <div class="flex items-center ml-4 text-xs font-bold text-gray-400">
-                                    @if ($week['disabled'])
-                                        <button wire:click="enable({{ $week['code'] }})"
-                                            class="bg-gray-100 px-2 rounded-l-full @if ($week['week_end']['status'] == true) hover:bg-green-100 hover:text-green-400 transition @endif"
-                                            @if ($week['week_end']['status'] == false) disabled @endif>
-                                            {{ __('On') }}
-                                        </button>
-                                    @else
-                                        <button class="px-2 rounded-l-full bg-green-100 text-green-400" disabled>
-                                            {{ __('On') }}
-                                        </button>
-                                    @endif
-
-                                    @if ($week['disabled'])
-                                        <button class="px-2 rounded-r-full bg-red-100 text-red-400 transition" disabled>
-                                            {{ __('Off') }}
-                                        </button>
-                                    @else
-                                        <button wire:click="disable({{ $week['code'] }})"
-                                            class="bg-gray-100 px-2 rounded-r-full @if ($week['week_end']['status'] == true) hover:bg-red-100 hover:text-red-400 transition @endif"
-                                            @if ($week['week_end']['status'] == false) disabled @endif>
-                                            {{ __('Off') }}
-                                        </button>
-                                    @endif
-                                </div>
-                            </td>
+                            <th scope="col" class="px-6 py-3 bg-gray-50 text-right tracking-wider">
+                                {{ __('Status') }}
+                            </th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @foreach ($roundWeeks as $week)
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                    {{ $loop->iteration }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 font-bold text-center">
+                                    # {{ $week['week_start']['number'] }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-xs text-gray-600 ml-4 text-center">
+                                    {{ $week['week_start']['human_date'] }} - {{ $week['week_end']['human_date'] }}
+                                <td
+                                    class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 text-right flex justify-end">
+                                    @if ($week['week_start']['status'] === true and $week['week_end']['status'] === false)
+                                        <x-admin.elements.badge-disabled>
+                                            {{ __('passed') }}
+                                        </x-admin.elements.badge-disabled>
+                                    @elseif ($week['week_start']['status'] === true and $week['week_end']['status'] === true)
+                                        <x-admin.elements.badge-success>
+                                            {{ __('current') }}
+                                        </x-admin.elements.badge-success>
+                                    @elseif ($week['week_start']['status'] === false and $week['week_end']['status'] === true)
+                                        <x-admin.elements.badge-primary>
+                                            {{ __('waiting') }}
+                                        </x-admin.elements.badge-primary>
+                                    @else
+                                        <x-admin.elements.badge-error>
+                                            {{ __('error') }}
+                                        </x-admin.elements.badge-error>
+                                    @endif
+
+                                    <div class="flex items-center ml-4 text-xs font-bold text-gray-400">
+                                        @if ($week['disabled'])
+                                            <button wire:click="enable({{ $week['code'] }})"
+                                                class="bg-gray-100 px-2 rounded-l-full @if ($week['week_end']['status'] == true) hover:bg-green-100 hover:text-green-400 transition @endif"
+                                                @if ($week['week_end']['status'] == false) disabled @endif>
+                                                {{ __('On') }}
+                                            </button>
+                                        @else
+                                            <button class="px-2 rounded-l-full bg-green-100 text-green-400" disabled>
+                                                {{ __('On') }}
+                                            </button>
+                                        @endif
+
+                                        @if ($week['disabled'])
+                                            <button class="px-2 rounded-r-full bg-red-100 text-red-400 transition"
+                                                disabled>
+                                                {{ __('Off') }}
+                                            </button>
+                                        @else
+                                            <button wire:click="disable({{ $week['code'] }})"
+                                                class="bg-gray-100 px-2 rounded-r-full @if ($week['week_end']['status'] == true) hover:bg-red-100 hover:text-red-400 transition @endif"
+                                                @if ($week['week_end']['status'] == false) disabled @endif>
+                                                {{ __('Off') }}
+                                            </button>
+                                        @endif
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <div class="p-4">
+                    <p class="text-center text-lg italic text-gray-500">
+                        {{ __('You must to select round to display data') }}
+                    </p>
+                </div>
+            @endif
         </div>
     </x-slot>
 </x-admin.page.details-section>

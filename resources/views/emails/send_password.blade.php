226dd274d353dcd0b3c0ae21e946ed67762795c3
Takhir Berdyiev <takhir.berdyiev@gmail.com>
2023-01-25 06:12:55 +0200

    Add reset password email translation

--- resources/views/emails/send_password.blade.php
+++ resources/views/emails/send_password.blade.php
@@ -1,5 +1,5 @@
 <x-mail::message>
-# {{ __('Dear stockholder, ') }} <i>{{ $name }}</i>
+# {{ __('Dear stockholder') }} <i>{{ $name }}</i>
 
 <p>{{ __('Here is your login information for our prioritization system, where you can enter up to 20 wishes in the upcoming prioritization round.') }}</p>
 <p>{{ __('You have received information about the homes in a separate email, and can also see our homes on the website.') }}</p>

03f34ba9e7b6f041a7f548798e37a8c9c733efed
Takhir Berdyiev <takhir.berdyiev@gmail.com>
2023-01-29 08:16:46 +0200

    Clean properties table fix code

--- app/Http/Livewire/Wishes/Table.php
+++ app/Http/Livewire/Wishes/Table.php
@@ -35,7 +35,6 @@ class Table extends Component
 
     public function render()
     {
-        // $this->databaseFix();
         sleep(1);
         $wishes = ($this->roundId) ? $this->getRoundWishes() : $this->getAllWishes();
         return view('livewire.wishes.table', [
@@ -76,64 +75,5 @@ class Table extends Component
     {
         $wishes = new Wish();
         return $wishes->query()->where('round_id', $this->roundId)->orderBy('created_at', 'desc');
-    }
-
-    public function databaseFix()
-    {
-        $properties = DB::table('properties_old')
-            // ->join('round_user', 'wishes.user_id', '=', 'round_user.user_id')
-            // ->join('users', 'round_user.user_id', '=', 'users.id')
-            // ->join('properties', 'wishes.property_id', '=', 'properties.id')
-            // ->select(
-            //     'wishes.id as id',
-            //     'wishes.status as status',
-            //     'wishes.priority as priority',
-            //     'wishes.week_code as week_code',
-            //     'wishes.user_id as user_id',
-            //     'round_user.priority as user_priority',
-            //     'users.name as user_name',
-            //     'wishes.property_id as property_id',
-            //     'properties.name as property_name'
-            // )
-            // ->where('wishes.round_id', $round->id)
-            // ->orderBy('round_user.priority')
-            ->get();
-
-        foreach ($properties as $property) :
-            $wishesCount = Wish::query()->where('property_id', $property->id)->count();
-            $hasBy = 'none';
-            $byName = Property::query()->where('name', $property->name)->first();
-            // $byAddress = Property::query()->where('address', 'Via Nazionale nr.70  28824 Oggebbio VB')->first();
-
-            if ($byName) :
-                if ($byName->name == $property->name) $hasBy = 'name';
-                Wish::query()->where('property_id', $property->id)->update(['property_id' => $byName->id]);
-            endif;
-
-            // if ($byAddress) :
-            //     if ($byAddress->address == $property->address) $hasBy = 'address';
-            // endif;
-
-            if ($hasBy == 'none') :
-                Property::query()->create([
-                    'name' => $property->name,
-                    'country' => $property->country,
-                    'address' => $property->address,
-                    'description' => $property->description,
-                    'deleted_at' => now(),
-                ]);
-            endif;
-
-            if ($hasBy == 'none' and $wishesCount > 0) :
-            // debugbar()->info("Property: $property->name ($property->id) :: Wishes($wishesCount) :: Has by: $hasBy");
-            endif;
-        endforeach;
-
-        foreach ($properties as $property) :
-            Wish::query()->where('property_id', $property->id)->update(['property_id' => $byName->id]);
-            if ($wishesCount > 0) :
-                debugbar()->info("Property: $property->name ($property->id) :: Wishes($wishesCount)");
-            endif;
-        endforeach;
     }
 }
